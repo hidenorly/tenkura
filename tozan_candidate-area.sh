8c9e5ab3fcf61aea8b64c105a9ffecb7e58563f5
hidenorly <twitte.harold@gmail.com>
2023-09-23 00:27:02 +0900

    Add tozan_candidate-area.sh

---
+++ tozan_candidate-area.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export _ARGS=`echo $@`; find . -maxdepth 1 -type f | grep -E "\.csv$" | grep -v "yama.00\.csv" | grep -v "exclude" | xargs -n 1 /bin/bash -c 'TMP=$1; printf "%-20s: %3d / %3d\n" "$TMP" $(python3 tenkura_get_weather.py -c -a A -t 6-15 $_ARGS -nu -i $TMP -e ~/exclude.csv | grep " A" | wc -l) $(cat $1 | wc -l)' /bin/bash
