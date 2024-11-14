95f921e304b75031e8a382b67ba25699066acdc1
hidenorly <twitte.harold@gmail.com>
2024-11-15 00:04:25 +0900

    Use config.sh on list-candidate.sh

--- list-candidate.sh
+++ list-candidate.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-POS_LONGITUDE="139.744972"
-POS_LATITUDE="35.675889"
+source config.sh
 MOUNTAINS=`./list-mountains.sh`
-echo $MOUNTAINS | xargs python3 ~/bin/tenkura_get_weather.py -a A,B -c -m -w rain,thunder -t 6-15 -dw -c -ns
+echo $MOUNTAINS | xargs python3 $GET_TENKURA_WEATHER -a A,B -c -m -w rain,thunder -t 6-15 -dw -c -ns
