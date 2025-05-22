43175c6474ee9ed6e691bd1e44abcbd518f10937
hidenorly <twitte.harold@gmail.com>
2025-05-22 20:53:37 +0900

    Add retry for get_weather_mountain.py

--- get_weather_from_weathernews.sh
+++ get_weather_from_weathernews.sh
@@ -2,4 +2,19 @@
 cd ~/tmp
 cp error.png saturday.png
 cp error.png sunday.png
-python3 ~/bin/get_weather_mountain.py -dw
+
+MAX_RETRIES=3
+RETRY_COUNT=0
+
+while [ $RETRY_COUNT -lt $MAX_RETRIES ]; do
+    python3 ~/bin/get_weather_mountain.py -dw
+    EXIT_CODE=$?
+
+    if [ $EXIT_CODE -eq 0 ]; then
+        break
+    else
+        RETRY_COUNT=$((RETRY_COUNT+1))
+        echo "retry...$RETRY_COUNT/$MAX_RETRIES"
+        sleep 1
+    fi
+done
